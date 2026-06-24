731ca4a00e9980c794a44b243a6c68962c551e74
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2026-06-24 10:59:28 +0200

    Creacion y Preparacion de la pagina (componente)
    Creacion y Preparacion de la pagina (componente)

--- 02-bases/src/app/app.routes.ts
+++ 02-bases/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterPage } from './pages/counter/counter-page';
 import { HeroPage } from './pages/hero/hero-page';
+import { DragonballPage } from './pages/dragonball/dragonball-page';
 
 export const routes: Routes = [
 
@@ -8,14 +9,20 @@ export const routes: Routes = [
         path: '',
         component: CounterPage
     },
-    
+
     {
         path: 'hero',
         component: HeroPage
     },
+
+    {
+        path: 'dragonball',
+        component: DragonballPage
+    },
+
     {
         path: '**',
         redirectTo: ''
-    }
-
+    },
+    
 ];
---
+++ 02-bases/src/app/pages/dragonball/dragonball-page.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball',
+  imports: [],
+  templateUrl: './dragonball-page.html',
+  styles: ``,
+})
+export class DragonballPage {}
