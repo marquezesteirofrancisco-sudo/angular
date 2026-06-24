a3b2be940316465734b00adbc73c81f14083b091
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2026-06-24 12:05:21 +0200

    54. Inputs - Agregar personaje
    54. Inputs - Agregar personaje

--- 02-bases/src/app/pages/dragonball/dragonball-page.ts
+++ 02-bases/src/app/pages/dragonball/dragonball-page.ts
@@ -19,33 +19,39 @@ interface Character {
 })
 export class DragonballPage {
 
+  name = signal('Gohan');
+  power = signal(100);
+
   characteres = signal<Character[]> ([
-    {
-      id : 1,
-      name : 'Goku',
-      power : 9001
-    },
-    
-    {
-      id : 2,
-      name : 'Vegeta',
-      power : 9000
-    },
-
-    {
-      id : 4,
-      name : 'Ymacha',
-      power : 500
-    },
-
-    
-    {
-      id : 3,
-      name : 'Piccolo',
-      power : 8003
-    },
-
+    {id : 1,name : 'Goku',power : 9001},
+    {id : 2,name : 'Vegeta',power : 9000},
+    {id : 4,name : 'Ymacha',power : 500},
+    {id : 3,name : 'Piccolo',power : 8003},
   ]);
+
+
+  addCharacter() {
+
+    if ( !this.name() || !this.power() ||  this.power() < 0)
+      return;
+
+    const newCharacter: Character =  {
+        id : this.characteres().length + 1,
+        name : this.name(),
+        power : this.power()
+    }
+
+    this.characteres.update((list) => [...list, newCharacter])  ;
+
+    this.resetField();
+
+    console.log( this.name(), this.power());
+  }
+
+  resetField() {
+    this.name.set('');
+    this.power.set(0);
+  }
 
 
 /*   powerClasses = computed(() => {
