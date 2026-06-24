f2970801edb0d50c7cf43a697dba3ad14b578062
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2026-06-24 19:49:31 +0200

    62. LinkedSignal - Cargar del LocalStorage
    62. LinkedSignal - Cargar del LocalStorage

--- 02-bases/src/app/services/dragonball.service.ts
+++ 02-bases/src/app/services/dragonball.service.ts
@@ -2,13 +2,18 @@ import { effect, Injectable, signal } from "@angular/core";
 import { Character } from "../interfaces/character.interface";
 
 
+const loadFromLocalStorage = () : Character[] => {
+
+  const characters = localStorage.getItem('characters')
+
+  return characters ? JSON.parse(characters) : [];
+  
+}
+
 @Injectable({ providedIn: 'root' })
 export class DragonballService {
 
-  characters = signal<Character[]> ([
-    {id : 1,name : 'Goku',power : 9001},
-    {id : 2,name : 'Vegeta',power : 9000}
-  ]);
+  characters = signal<Character[]> (loadFromLocalStorage());
 
 
   addCharacter(character: Character) {
@@ -18,7 +23,7 @@ export class DragonballService {
   }
 
   saveToLocalStore = effect(()=>{
-      localStorage.setItem("characters", JSON.stringify(this.characters()))
+      localStorage.setItem('characters', JSON.stringify(this.characters()))
   });
 
 
