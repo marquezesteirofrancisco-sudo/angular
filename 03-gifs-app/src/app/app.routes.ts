831b98fb2bcaacc50b5b26039e024221101c678a
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2026-06-30 12:30:30 +0200

    69. Pensemos en componentes
    69. Pensemos en componentes

--- 03-gifs-app/src/app/app.routes.ts
+++ 03-gifs-app/src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path : 'dashboard',
+        loadComponent: () => import ('./gifs/pages/dashboard-pages/dashboard-pages'),
+    },
+
+
+    {
+        path : 'trending',
+        loadComponent: () => import ('./gifs/pages/trending-page/trending-page'),
+    },
+
+    {
+        path : 'search',
+        loadComponent: () => import ('./gifs/pages/search-page/search-page'),
+    },
+
+
+    {
+        path: '**',
+        redirectTo: 'dashboard'
+    }
+];
---
+++ 03-gifs-app/src/app/gifs/pages/dashboard-pages/dashboard-pages.ts
@@ -0,0 +1,8 @@
+import {  Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-pages',
+  imports: [],
+  templateUrl: './dashboard-pages.html',
+})
+export default class DashboardPages {}
