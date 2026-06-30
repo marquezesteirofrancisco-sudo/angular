b0607190ea27ae1f62cb99769450d2f5549855fc
Francisco Marquez Esteiro <117361707+franciscomes@users.noreply.github.com>
2026-06-30 13:50:56 +0200

    72. RouterLinks - Desde componentes hijos
    72. RouterLinks - Desde componentes hijos

--- 03-gifs-app/src/app/gifs/components/side-menu-options/side-menu-options.ts
+++ 03-gifs-app/src/app/gifs/components/side-menu-options/side-menu-options.ts
@@ -1,8 +1,36 @@
 import {  Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from "@angular/router";
+
+interface MenuOption {
+  icon: string,
+  label:string,
+  route:string,
+  subLabel:string,
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.html',
 })
-export class SideMenuOptions {}
+export class SideMenuOptions {
+
+  menuOptions : MenuOption[] = [
+
+    { 
+      icon : 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populares',
+      route:'/dashboard/trending'
+    },
+
+    { 
+      icon : 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Gifs Gifs',
+      route:'/dashboard/search'
+    },
+
+  ]
+
+}
